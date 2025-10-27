42cc3715f055d83d3ec0431201feb559f4db31a3
Anis <mdanismia992@gmail.com>
2025-10-27 08:15:18 +0600

    chore: add resources

---
+++ src/components/icons/sun-icon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M10.5 14.5C12.7091 14.5 14.5 12.7091 14.5 10.5C14.5 8.29086 12.7091 6.5 10.5 6.5C8.29086 6.5 6.5 8.29086 6.5 10.5C6.5 12.7091 8.29086 14.5 10.5 14.5Z"
+      fill="currentColor"
+    />
+    <path d="M10.5 0.5V2.5V0.5Z" fill="currentColor" />
+    <path d="M10.5 18.5V20.5V18.5Z" fill="currentColor" />
+    <path d="M3.42993 3.42993L4.83993 4.83993L3.42993 3.42993Z" fill="currentColor" />
+    <path d="M16.1599 16.1599L17.5699 17.5699L16.1599 16.1599Z" fill="currentColor" />
+    <path d="M0.5 10.5H2.5H0.5Z" fill="currentColor" />
+    <path d="M18.5 10.5H20.5H18.5Z" fill="currentColor" />
+    <path d="M4.83993 16.1599L3.42993 17.5699L4.83993 16.1599Z" fill="currentColor" />
+    <path d="M17.5699 3.42993L16.1599 4.83993L17.5699 3.42993Z" fill="currentColor" />
+    <path
+      d="M10.5 0.5V2.5M10.5 18.5V20.5M3.42993 3.42993L4.83993 4.83993M16.1599 16.1599L17.5699 17.5699M0.5 10.5H2.5M18.5 10.5H20.5M4.83993 16.1599L3.42993 17.5699M17.5699 3.42993L16.1599 4.83993M14.5 10.5C14.5 12.7091 12.7091 14.5 10.5 14.5C8.29086 14.5 6.5 12.7091 6.5 10.5C6.5 8.29086 8.29086 6.5 10.5 6.5C12.7091 6.5 14.5 8.29086 14.5 10.5Z"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
