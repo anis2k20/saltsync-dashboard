10656d6326c8e39adde14d9b4f3519ab2c5a0e54
Anis <mdanismia992@gmail.com>
2025-10-27 08:12:09 +0600

    feat: add referral reward section

---
+++ src/components/referral/RewardPointCard.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import Button from '@/components/ui/Button.vue'
+</script>
+
+<template>
+  <div
+    class="relative flex h-[170px] flex-1 flex-col items-center justify-center overflow-hidden rounded-xl border border-border-primary bg-white px-lg py-base shadow"
+  >
+    <img
+      src="@/assets/images/gradient-small-1.svg"
+      alt="gradient"
+      class="absolute top-0 left-0 w-56 border blur-2xl"
+    />
+    <img
+      src="@/assets/images/gradient-small-2.svg"
+      alt="gradient"
+      class="absolute top-0 left-0 w-60 border opacity-50 blur-2xl"
+    />
+    <div class="relative z-10 flex w-full items-center gap-3">
+      <img src="@/assets/images/toffee.webp" alt="toffee" class="h-20" />
+      <div class="w-full space-y-2 text-text-quaternary-light">
+        <p class="text-xl font-medium">Reward Point</p>
+        <div class="flex w-full items-center justify-between">
+          <div>
+            <span class="font-secondary text-4xl font-semibold text-primary-light">200</span>
+            <span class="ml-2 text-2xl">৳ 2000</span>
+          </div>
+          <Button class="!bg-brand-solid text-white">Upgrade</Button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/referral/StateCard.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="relative flex h-[100px] flex-1 flex-col items-center justify-center overflow-hidden rounded-xl border border-border-primary bg-white px-lg py-base shadow"
+  >
+    <img
+      src="@/assets/images/gradient-small-1.svg"
+      alt="gradient"
+      class="absolute -top-20 left-20 h-72 w-56 border blur-2xl"
+    />
+    <img
+      src="@/assets/images/gradient-small-2.svg"
+      alt="gradient"
+      class="absolute -left-10 w-60 border opacity-50 blur-2xl"
+    />
+    <div class="relative z-10 flex w-full items-center gap-3">
+      <div
+        class="flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-brand-secondary/50"
+      ></div>
+      <div class="w-full space-y-2 text-text-quaternary-light">
+        <p class="text-md font-semibold">Reward Point</p>
+
+        <h3 class="font-secondary text-4xl font-semibold text-primary-light">200</h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
